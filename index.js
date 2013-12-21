73a6de822e3c5f38c18e5426fbc0320acfaad923
Fritz-Lium <fritzlium@gmail.com>
2013-12-21 17:17:53 +0800

    use callback

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var mysql = require('mysql');
 var fs = require('fs');
 var config = require('./config');
 
-function execsql(filename) {
+function execsql(filename, callback) {
 	fs.readFile(filename, 'utf8', function (err, data) {
 		if (err) throw err;
 
 		var conn = mysql.createConnection(config);
 		conn.connect();
 		conn.query(data, function (err, results) {
-			if (err) throw err;
-			for (var i = 0; i < results.length; i++) {
-				console.log(i + ':' + JSON.stringify(results[i]));
-			}
+			callback(err, results);
 		});
 		conn.end();
 	});
