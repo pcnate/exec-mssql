44fb4c3f1924dec1582cc9e651acff70899e610f
Fritz-Lium <fritzlium@gmail.com>
2013-12-22 00:09:07 +0800

    can config `user` and `password`

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-var mysql = require('mysql');
-var fs = require('fs');
-var config = require('./config');
+var fs = require('fs'),
+	_ = require('underscore'),
+	mysql = require('mysql'),
+	config = {
+		multipleStatements: true,
+		host: 'localhost'
+	};
 
-function execsql(filename, callback) {
+function exec(filename, callback) {
 	fs.readFile(filename, 'utf8', function (err, data) {
 		if (err) throw err;
-
 		var conn = mysql.createConnection(config);
 		conn.connect();
 		conn.query(data, function (err, results) {
+			if (!_.isArray(results)) {
+				results = [results];
+			}
 			callback(err, results);
 		});
 		conn.end();
 	});
+	return this;
 }
 
-module.exports = execsql;
+exports.exec = exec;
+exports.config = function (options) {
+	_.extend(config, _.pick(options, ['user', 'password']));
+	return this;
+}
