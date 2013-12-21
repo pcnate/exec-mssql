ed4ed7deb5c39d51ab8218eb86557c6a3b599052
Fritz-Lium <fritzlium@gmail.com>
2013-12-21 17:10:19 +0800

    format the code

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var mysql = require('mysql');
 var fs = require('fs');
 var config = require('./config');
 
-function execsql( filename ){
-fs.readFile(filename, 'utf8', function(err, data){
-	if(err) throw err;
-	
-	var conn = mysql.createConnection( config );
-	conn.connect();
-	conn.query(data, function(err, results){
-		if(err) throw err;
-		for(var i=0; i<results.length; i++){
-			console.log( i + ':' + JSON.stringify(results[i]) );
-		}
+function execsql(filename) {
+	fs.readFile(filename, 'utf8', function (err, data) {
+		if (err) throw err;
+
+		var conn = mysql.createConnection(config);
+		conn.connect();
+		conn.query(data, function (err, results) {
+			if (err) throw err;
+			for (var i = 0; i < results.length; i++) {
+				console.log(i + ':' + JSON.stringify(results[i]));
+			}
+		});
+		conn.end();
 	});
-	conn.end();
-});
 }
 
 module.exports = execsql;
